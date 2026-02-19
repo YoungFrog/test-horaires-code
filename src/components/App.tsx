4c60b5e87c4296f5bac82e3c42000d0502d1db70
Nicolas Richard <nrichard@he2b.be>
2026-02-19 11:57:54 +0100

    Fix slotEventOverlap prop syntax in App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -161,7 +161,7 @@ const App = (config: CalendarConfig): JSX.Element => {
           }}
           slotMinTime="08:00:00"
           slotMaxTime="22:00:00"
-          slotEventOverlap=false
+          slotEventOverlap={false}
           businessHours={{
             dayOfWeek: [1, 2, 3, 4, 5],
             startTime: '08:15',
